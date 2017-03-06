236bbeb17de7058a2af18bc8bd192a1881f9c1de
Seungyeon Kim <seung6870@gmail.com>
2017-03-06 15:59:48 +0900

    HUMANBOOK_SERVICE_DAY 관계 테이블 컬럼명 변경, HumanbookRepository select -> service_day 셋팅 추

--- humanlibrary-core/src/main/java/kr/withever/humanlibrary/repo/HumanbookRepository.java
+++ humanlibrary-core/src/main/java/kr/withever/humanlibrary/repo/HumanbookRepository.java
@@ -1,10 +1,13 @@
 package kr.withever.humanlibrary.repo;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import kr.withever.humanlibrary.domain.humanbook.Humanbook;
 import kr.withever.humanlibrary.repo.mapper.HumanbookMapper;
+import kr.withever.humanlibrary.repo.mapper.HumanbookServiceDayMapper;
 
 @Repository
 public class HumanbookRepository {
@@ -12,13 +15,24 @@ public class HumanbookRepository {
 	@Autowired
 	private HumanbookMapper humanbookMapper;
 	
+	@Autowired
+	private HumanbookServiceDayMapper humanbookServiceDayMapper;
+	
 	public Humanbook retrieveHumanbook(Long id){
 		Humanbook humanbook = this.humanbookMapper.selectHumanbook(id);
+		if(humanbook != null){
+			Set<String> dayList = this.humanbookServiceDayMapper.selectHumanbookServiceDayList(id);
+			humanbook.setServiceDay(dayList);
+		}
 		return humanbook;
 	}
 	
 	public Humanbook retrieveHumanbookByUserId(String userId){
 		Humanbook humanbook = this.humanbookMapper.selectHumanbookByUserId(userId);
+		if(humanbook != null){
+			Set<String> dayList = this.humanbookServiceDayMapper.selectHumanbookServiceDayList(humanbook.getId());
+			humanbook.setServiceDay(dayList);
+		}
 		return humanbook;
 	}
 	
