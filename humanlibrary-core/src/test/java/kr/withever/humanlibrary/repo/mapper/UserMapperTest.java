6f2875675cd092c5ee16b44ddd0f0a7939d31320
youngjinkim <youngjinguy@gmail.com>
2017-03-08 15:10:34 +0900

    * 사용자 CRUD / TEST 추가.

--- humanlibrary-core/src/test/java/kr/withever/humanlibrary/repo/mapper/UserMapperTest.java
+++ humanlibrary-core/src/test/java/kr/withever/humanlibrary/repo/mapper/UserMapperTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by youngjinkim on 2017. 2. 10..
@@ -22,8 +23,45 @@ public class UserMapperTest extends WitheverDbUnitTestConfig {
     private UserMapper userMapper;
 
     @Test
+    public void insertUser() throws Exception {
+        User user = new User("yjkim", "김영진", "jin@gmail.com", "123456");
+        this.userMapper.insertUser(user);
+
+        User insertedUser = this.userMapper.selectUser(2L);
+        assertEquals(user.getLoginId(), insertedUser.getLoginId());
+        assertEquals(user.getName(), insertedUser.getName());
+        assertEquals(user.getEmail(), insertedUser.getEmail());
+        assertEquals(user.getPassword(), insertedUser.getPassword());
+    }
+
+    @Test
     public void selectUser() throws Exception {
         User user = this.userMapper.selectUser(1L);
+        assertEquals("jin", user.getLoginId());
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+        User user = this.userMapper.selectUser(1L);
+        user.setName("jinjin");
+        user.setAddress("1234");
+        this.userMapper.updateUser(user);
+
+        User updatedUser = this.userMapper.selectUser(1L);
+        assertEquals(user.getName(), updatedUser.getName());
+        assertEquals(user.getAddress(), updatedUser.getAddress());
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        this.userMapper.deleteUser(1L);
+        User user = this.userMapper.selectUser(1L);
+        assertNull(user);
+    }
+
+    @Test
+    public void selectUserByLoginId() throws Exception {
+        User user = this.userMapper.selectUserByLoginId("jin");
         assertEquals("jin", user.getLoginId());
     }
 
