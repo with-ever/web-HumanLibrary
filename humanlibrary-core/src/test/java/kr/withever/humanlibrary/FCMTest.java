af0225131a6d469b452c8ef64c3f3735d8da8adf
youngjinkim <youngjinguy@gmail.com>
2017-06-08 11:45:04 +0900

    * 푸시 테스트 이그노어 처

--- humanlibrary-core/src/test/java/kr/withever/humanlibrary/FCMTest.java
+++ humanlibrary-core/src/test/java/kr/withever/humanlibrary/FCMTest.java
@@ -3,6 +3,7 @@ package kr.withever.humanlibrary;
 import kr.withever.humanlibrary.domain.common.client.FCMData;
 import kr.withever.humanlibrary.domain.common.client.FCMNotification;
 import kr.withever.humanlibrary.util.FCMUtil;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class FCMTest {
     private static final String TEST_TOKEN_ID = "fNo8l-lwtMI:APA91bF4bxMrk7moceQWDu9KSO6N4HVGYn9L6MnQjjV3hJxisnWBOTuQ1JVQADf0RZ3XxOHwQhWp9ykirYUWbEQsBlWkQvyvWlERq7jpdgZ1CZoCqZePs9DdHp6cTF59wRTe9wdLcUYY";
 
     @Test
+    @Ignore
     public void sendMessage() throws IOException {
         FCMUtil.sendMessage(TEST_TOKEN_ID, new FCMNotification("title test","message test"), FCMData.contract("5"));
     }
